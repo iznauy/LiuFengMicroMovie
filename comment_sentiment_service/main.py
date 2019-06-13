3f3ed5297633fcb82a3a73b8af098275516b788f
yuan <zy05160516@126.com>
2019-06-13 11:10:13 +0800

    add comment_sentiment_service

---
+++ comment_sentiment_service/main.py
@@ -0,0 +1,21 @@
+import json
+
+from flask import Flask, request
+
+from sentiment import sentiment
+
+app = Flask(__name__)
+default_name = 'Comment Sentiment Service'
+
+@app.route('/', methods=['POST'])
+def get_comment_sentiments():
+    comments = json.loads(request.form['comments'])
+    tags = []
+    for comment in comments:
+        tag = sentiment(comment)
+        tags.append(tag)
+    return json.dumps(tags)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
